31d9a5300b76adee94e7afec23a02f450ddd7cd7
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-11-27 21:12:47 +0200

    done

--- app/src/main/java/com/example/taskmaster/MainActivity2.java
+++ app/src/main/java/com/example/taskmaster/MainActivity2.java
@@ -2,35 +2,29 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
-import androidx.loader.content.AsyncTaskLoader;
-import androidx.room.Room;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.List;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
 
 public class MainActivity2 extends AppCompatActivity {
-   private TaskRoomDatabase db;
+//   private TaskRoomDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         EditText editText1 = findViewById(R.id.editTitleId);
-        EditText editText2 = findViewById(R.id.desId);
-
-        db =  TaskRoomDatabase.taskRoomInstance;
-//        TaskRoomDatabase db = Room.databaseBuilder(getApplicationContext(),TaskRoomDatabase.class,"task").allowMainThreadQueries().fallbackToDestructiveMigration().build();
-
-
-
+        EditText editText2 = findViewById(R.id.bodyId);
+        EditText editText3 = findViewById(R.id.stateId2);
 
 
 
@@ -45,24 +39,22 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-//                TaskDao taskDao = db.taskDao();
-//                taskDao.insert(new Task("myTitle","muyBody","myState"));
-//                List<Task> tasks = taskDao.getAll().getValue();
-//                System.out.println(tasks);
+                String title = editText1.getText().toString();
+                String body = editText2.getText().toString();
+                String state = editText3.getText().toString();
 
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        String myTitel1 = editText1.getText().toString();
-                        String myTitel2 = editText2.getText().toString();
-                        TaskDao taskDao = db.taskDao();
-                    taskDao.insert(new Task(myTitel1,myTitel2,"new"));
-                    List<Task> tasks = taskDao.getAll();
-                    System.out.println(tasks);
-                    }
-                });
                 Toast.makeText(getApplicationContext(),"Submittet",Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+               // startActivity(new Intent(MainActivity2.this,MainActivity.class));
+
+                // builder method to create an object to take the user title , body,state (that entered from the form and pass them here)
+                Task task = Task.builder().title(title).body(body).state(state).build();
+
+                // mutate to write to the db (save the data)
+                Amplify.API.mutate(
+                        ModelMutation.create(task),
+                        response -> Log.i("TaskMaster", "Added Task with id: " + response.getData().getId()),
+                        error -> Log.e("TaskMaster", "Create failed", error)
+                );
             }
         });
 
--- app/src/main/java/com/example/taskmaster/TaskRoomDatabase.java
+++ app/src/main/java/com/example/taskmaster/TaskRoomDatabase.java
@@ -1,27 +1,27 @@
-package com.example.taskmaster;
-
-import android.content.Context;
-
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Task.class} ,version = 2)
-public abstract class TaskRoomDatabase extends RoomDatabase {
-
-    public abstract TaskDao taskDao();
-
-    public static  TaskRoomDatabase taskRoomInstance;
-
-     static TaskRoomDatabase getData(final Context context) {
-         if (taskRoomInstance == null) {
-             synchronized (TaskRoomDatabase.class) {
-                 if (taskRoomInstance == null) {
-                     taskRoomInstance = Room.databaseBuilder(context.getApplicationContext(), TaskRoomDatabase.class, "task").build();
-                 }
-             }
-         }
-         return taskRoomInstance;
-     }
-     }
-
+//package com.example.taskmaster;
+//
+//import android.content.Context;
+//
+//import androidx.room.Database;
+//import androidx.room.Room;
+//import androidx.room.RoomDatabase;
+//
+//@Database(entities = {Task.class} ,version = 2)
+//public abstract class TaskRoomDatabase extends RoomDatabase {
+//
+//    public abstract TaskDao taskDao();
+//
+//    public static  TaskRoomDatabase taskRoomInstance;
+//
+//     static TaskRoomDatabase getData(final Context context) {
+//         if (taskRoomInstance == null) {
+//             synchronized (TaskRoomDatabase.class) {
+//                 if (taskRoomInstance == null) {
+//                     taskRoomInstance = Room.databaseBuilder(context.getApplicationContext(), TaskRoomDatabase.class, "task").build();
+//                 }
+//             }
+//         }
+//         return taskRoomInstance;
+//     }
+//     }
+//
